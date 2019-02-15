ce4d463b2ae00778118dc612da69c509e68e1194
Gaston Aganza <gaganza@calpoly.edu>
2019-02-14 23:00:57 -0800

    Added route for Dashboard and a mock module

--- chess/frontend/gui/src/App.js
+++ chess/frontend/gui/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import User from "./modules/ViewUser/User";
 import SignIn from "./modules/SignIn/SignIn";
+import Dashboard from "./modules/Dashboard/Dashboard";
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
         <Switch>
           <Route path="/users" component={User} />
           <Route path="/sign_in" component={SignIn} />
+          <Route path="/dashboard" component={Dashboard} />
         </Switch>
       </BrowserRouter>
     );
