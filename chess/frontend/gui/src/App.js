5f366cb35597e49976f775622acdcd2e43ecea65
Gaston Aganza <gaganza@calpoly.edu>
2019-02-14 21:27:58 -0800

    Fixed dependencies and added login

--- chess/frontend/gui/src/App.js
+++ chess/frontend/gui/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
 import User from "./modules/ViewUser/User";
+import SignIn from "./modules/SignIn/SignIn";
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
           <Route path="/users" component={User} />
+          <Route path="/sign_in" component={SignIn} />
         </Switch>
       </BrowserRouter>
     );
