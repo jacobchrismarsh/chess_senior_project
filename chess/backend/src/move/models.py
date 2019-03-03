7564db1e7cb9536424e7c29b1b5203f620094684
Gaston Aganza <gaston.aganza@gmail.com>
2019-03-02 17:33:53 -0800

    Added moves table (#15)
    * Added moves table
    
    * Remvoed unnecessary files

--- chess/backend/src/move/models.py
+++ chess/backend/src/move/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+
+
+class Moves(models.Model):
+    id = models.AutoField(primary_key=True)
+    white_user_id = models.IntegerField()
+    black_user_id = models.IntegerField()
+    move_number = models.IntegerField()
+    pre_move_fen = models.CharField(max_length=128),
+    post_move_fen = models.CharField(max_length=128),
+    move_algebraic = models.CharField(max_length=32)
+    game_id = models.ForeignKey('game.Games', on_delete=models.CASCADE)
+    turn = models.CharField(max_length=8)
