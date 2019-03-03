da76efd48ff9db22cea301eb2a8c7bc4e3c45559
Gaston Aganza <gaganza@calpoly.edu>
2019-03-02 17:01:53 -0800

    Adding updated model

--- chess/backend/src/games/models.py
+++ chess/backend/src/games/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Game(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id_1 = models.IntegerField()
+    user_id_2 = models.IntegerField()
+    white_user_id = models.IntegerField()
+    black_user_id = models.IntegerField()
+    difficulty_level = models.IntegerField(default=None)
