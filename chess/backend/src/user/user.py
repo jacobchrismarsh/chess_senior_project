d2ef0ed9218e2097d0322e6dfe1c8144fb95d34f
Gaston Aganza <gaganza@calpoly.edu>
2019-05-29 12:37:09 -0700

    WIP

--- chess/backend/src/user/user.py
+++ chess/backend/src/user/user.py
@@ -2,29 +2,33 @@ from django.contrib.auth.models import User
 from django.core.handlers.wsgi import WSGIRequest
 from django.http.response import JsonResponse
 from django.contrib.auth import authenticate, login, logout
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponseRedirect
 
-
+@csrf_exempt
 def create_user(request: WSGIRequest) -> JsonResponse:
-    username = request.GET.get("username")
-    password = request.GET.get("password")
-    email = request.GET.get("email")
+    username = request.POST.get("username")
+    password = request.POST.get("password")
+    email = request.POST.get("email")
     user = User.objects.create_user(username=username, password=password, email=email)
     user.save()
     return JsonResponse({"status": "success"})
 
-
+@csrf_exempt
 def login_user(request: WSGIRequest) -> JsonResponse:
     user = authenticate_user(request)
 
     if user is not None:
         login(request, user)
-        return JsonResponse({"status": "success"})
+        return HttpResponseRedirect("http://localhost:3000/game/")
+        # return JsonResponse({"status": "success"})
     else:
         return JsonResponse({"status": "failure"})
 
+@csrf_exempt
 def authenticate_user(request: WSGIRequest) -> User:
-    username = request.GET.get("username")
-    password = request.GET.get("password")
+    username = request.POST.get("username")
+    password = request.POST.get("password")
     user = authenticate(username=username, password=password)
     return user
 
