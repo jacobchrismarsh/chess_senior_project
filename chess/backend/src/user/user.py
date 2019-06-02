6a3ab3e79ef31ea9f1de47d73edc12bdab96ee52
Jacob Marshall <Jacobcmarshall1856@gmail.com>
2019-06-02 15:20:25 -0700

    Add error to create_user if username already taken

--- chess/backend/src/user/user.py
+++ chess/backend/src/user/user.py
@@ -1,19 +1,31 @@
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.core.handlers.wsgi import WSGIRequest
-from django.http.response import JsonResponse
-from django.contrib.auth import authenticate, login, logout
-from django.views.decorators.csrf import csrf_exempt
+from django.db import IntegrityError
 from django.http import HttpResponseRedirect
+from django.http.response import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 @csrf_exempt
 def create_user(request: WSGIRequest) -> JsonResponse:
     username = request.POST.get("username")
     password = request.POST.get("password")
     email = request.POST.get("email")
-    user = User.objects.create_user(username=username, password=password, email=email)
-    if user.save():
-        return HttpResponseRedirect("http://localhost:3000/sign_in/")
-    return JsonResponse({"status": "failure"})
+
+    status = "success"
+    error = ""
+
+    try:
+        user = User.objects.create_user(
+            username=username, password=password, email=email
+        )
+    except IntegrityError:
+        status = "failure"
+        error = "That username is already taken"
+
+    return JsonResponse({"status": status, "error": error})
+
 
 @csrf_exempt
 def login_user(request: WSGIRequest) -> JsonResponse:
@@ -24,6 +36,7 @@ def login_user(request: WSGIRequest) -> JsonResponse:
         return HttpResponseRedirect("http://localhost:3000/dashboard/")
     else:
         return JsonResponse({"status": "failure"})
+
 
 @csrf_exempt
 def authenticate_user(request: WSGIRequest) -> User:
@@ -36,6 +49,7 @@ def authenticate_user(request: WSGIRequest) -> User:
 def logout_user(request: WSGIRequest) -> JsonResponse:
     logout(request)
     return JsonResponse({"status": "success"})
+
 
 def is_logged_in(request: WSGIRequest) -> JsonResponse:
     return JsonResponse({"status": request.user.is_authenticated})
