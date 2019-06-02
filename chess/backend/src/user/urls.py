b563f446c9599008cb62d66a7899cc2f4ca8997d
Jacob Marshall <Jacobcmarshall1856@gmail.com>
2019-06-02 13:51:00 -0700

    Check if a user is logged in or not

--- chess/backend/src/user/urls.py
+++ chess/backend/src/user/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("new_user/", user.create_user, name="create-user"),
     path("login/", user.login_user, name="login-user"),
     path("logout/", user.logout_user, name="logout-user"),
+    path("is_logged_in/", user.is_logged_in, name="is_logged_in"),
 ]
