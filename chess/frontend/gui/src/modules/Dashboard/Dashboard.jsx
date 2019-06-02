5628e50d6d3322dc049701957ac912bd55272999
Gaston Aganza <gaganza@calpoly.edu>
2019-06-02 15:48:04 -0700

    Updated Header CSS

--- chess/frontend/gui/src/modules/Dashboard/Dashboard.jsx
+++ chess/frontend/gui/src/modules/Dashboard/Dashboard.jsx
@@ -23,73 +23,24 @@ class Dashboard extends Component {
   constructor(props) {
     super(props);
 
-    this.state = { width: 0, height: 0 };
     autoBind(this);
   }
   
   componentDidMount() {
-    this.updateWindowDimensions();
-    window.addEventListener('resize', this.updateWindowDimensions);
-
     checkSignedIn().then(
       response => {
+        debugger;
         if (!response.status) {
           window.location ='/sign_in/'
         }
     });
   }
-  
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updateWindowDimensions);
-  }
-  
-  updateWindowDimensions() {
-    this.setState({ width: window.innerWidth, height: window.innerHeight });
-  }
 
-  getRandomNumberOfSamePolarity(num, len) {
-    let res = Math.trunc(Math.random() * len);
-    while (this.isEven(num) !== this.isEven(res)) {
-      res = Math.trunc(Math.random() * len);
-    }
-
-    return res;
-  }
-
-  isEven(num) {
-    if (num == 0) {
-      return true;
-    }
-    return num % 2 === 0;
-  }
-
-  renderBoarder() {
-    let pieces = [whitePawn, blackPawn, whiteBishop, blackBishop, whiteKnight, blackKnight, whiteRook,
-                  blackRook, whiteQueen, blackQueen, whiteKing, blackKing];
-    let { width } = this.state;
-    let widthOfPiece = 50;
-
-    let renderElements = [];
-    let rand = Math.random() * (pieces.length);
-
-    for (let i = 0; i < Math.floor(width / widthOfPiece); i++) {
-      renderElements.push(<Square key={i} piece={pieces[this.getRandomNumberOfSamePolarity(i, pieces.length)]} />);
-    }
-
-    return (
-      <div>
-        {renderElements}
-      </div>
-    );
-  }
 
   render() {
     
     return (
     <div>
-      <div className="dashboard-boarder">
-        {this.renderBoarder()}
-      </div>
       <div className="dashboard-container">
         <div className="dashboard-sidebar">
           <LeftSidebar />
