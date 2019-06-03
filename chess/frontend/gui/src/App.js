1ac226d0c38efad673fe14141300ddc7c3301a3c
Gaston Aganza <gaganza@calpoly.edu>
2019-06-02 17:48:05 -0700

    Hides the banner if you aren't logged in

--- chess/frontend/gui/src/App.js
+++ chess/frontend/gui/src/App.js
@@ -1,21 +1,49 @@
 import React, { Component } from "react";
 import { Route, Switch, BrowserRouter } from "react-router-dom";
+import { checkSignedIn } from "./modules/Common/utils";
 import SignIn from "./modules/SignIn/SignIn";
 import Dashboard from "./modules/Dashboard/Dashboard";
 import CreateUser from "./modules/CreateUser/CreateUser";
 import Game from "./modules/Game/Game";
+import Header from "./modules/Header/Header";
+import autoBind from "react-autobind";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      signedIn: false
+    }
+
+    autoBind(this);
+  }
+
+  componentDidMount() {
+    checkSignedIn().then(response => {
+      this.setState({
+        signedIn: response.status
+      })
+    });
+  }
+  
   render() {
     return (
-      <BrowserRouter>
-        <Switch>
-          <Route path="/sign_in" component={SignIn} />
-          <Route path="/create_user" component={CreateUser} />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/game" component={Game} />
-        </Switch>
-      </BrowserRouter>
+      <div>
+        {
+          this.state.signedIn && (
+            <Header />
+          )
+        }
+        <BrowserRouter>
+          <Switch>
+            <Route path="/sign_in" component={SignIn} />
+            <Route path="/create_user" component={CreateUser} />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/game" component={Game} />
+          </Switch>
+        </BrowserRouter>
+      </div>
     );
   }
 }
