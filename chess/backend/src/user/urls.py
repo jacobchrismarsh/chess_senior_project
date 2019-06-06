aa70630186a147738c9dd7adb91707f2f351f27d
Gaston Aganza <gaganza@calpoly.edu>
2019-06-05 19:12:35 -0700

    Helper function to find out who is sending a request

--- chess/backend/src/user/urls.py
+++ chess/backend/src/user/urls.py
@@ -2,10 +2,6 @@ from django.urls import path
 from . import user, views
 
 urlpatterns = [
-    path("new_user/", user.create_user, name="create-user"),
-    path("login/", user.login_user, name="login-user"),
-    path("logout/", user.logout_user, name="logout-user"),
-    path("is_logged_in/", user.is_logged_in, name="is_logged_in"),
     path('current_user/', views.current_user),
     path('users/', views.UserList.as_view())
 ]
--- chess/backend/src/user/user.py
+++ chess/backend/src/user/user.py
@@ -6,62 +6,9 @@ from django.db import IntegrityError
 from django.http import HttpResponseRedirect
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.request import Request
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 
-
-@csrf_exempt
-def create_user(request: WSGIRequest) -> JsonResponse:
-    username, password, email = _get_user_pass_email_from_request(request)
-    status = "success"
-    error = ""
-
-    try:
-        user = User.objects.create_user(
-            username=username, password=password, email=email
-        )
-    except IntegrityError:
-        status = "failure"
-        error = "That username is already taken"
-
-    return JsonResponse({"status": status, "error": error})
-
-
-def _get_user_pass_email_from_request(request: WSGIRequest) -> Tuple[str, str, str]:
-    username = request.POST.get("username")
-    password = request.POST.get("password")
-    email = request.POST.get("email")
-    return username, password, email
-
-
-
-@csrf_exempt
-def login_user(request: WSGIRequest) -> JsonResponse:
-    user = authenticate_user(request)
-
-    if user is not None:
-        login(request, user)
-        print(user.get_username())
-        return JsonResponse({"status": "success"})
-    else:
-        return JsonResponse(
-            {"status": "failure", "error": "Invalid username or password"}
-        )
-
-
-
-@csrf_exempt
-def authenticate_user(request: WSGIRequest) -> User:
-    username = request.POST.get("username")
-    password = request.POST.get("password")
-    user = authenticate(username=username, password=password)
-    return user
-
-
-def logout_user(request: WSGIRequest) -> JsonResponse:
-    logout(request)
-    return JsonResponse({"status": "success"})
-
-
-def is_logged_in(request: WSGIRequest) -> JsonResponse:
-    print(request.user.get_username())
-    return JsonResponse({"status": request.user.is_authenticated})
+def get_user_info(request: Request): 
+    return JSONWebTokenAuthentication().authenticate(Request(request))
