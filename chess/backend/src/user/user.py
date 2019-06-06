6776e730e4a4793e5b1031f1ee238c69f378ed66
Jacob Marshall <Jacobcmarshall1856@gmail.com>
2019-06-06 11:10:46 -0700

    Return user from get_user_info instead of tuple

--- chess/backend/src/user/user.py
+++ chess/backend/src/user/user.py
@@ -3,5 +3,5 @@ from rest_framework.request import Request
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 
-def get_user_info(request: Request) -> JSONWebTokenAuthentication:
-    return JSONWebTokenAuthentication().authenticate(Request(request))
+def get_user_info(request: Request) -> User:
+    return JSONWebTokenAuthentication().authenticate(Request(request))[0]
