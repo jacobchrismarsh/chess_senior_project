9430e500b14efb13aa87d0bf2ad261e2090841af
Jacob Marshall <Jacobcmarshall1856@gmail.com>
2019-06-09 22:25:27 -0700

    Add endpoint for getting active games

--- chess/backend/src/game/urls.py
+++ chess/backend/src/game/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("get_moves/", logic.get_all_moves, name="get_all_moves"),
     path("make_move/", logic.make_move, name="make_move"),
     path("get_opponent_move/", logic.get_opponent_move, name="get_opponent_move"),
+    path("get_current_games/", logic.get_current_games, name="get_current_games"),
 ]
